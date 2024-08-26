d7a2c1338557dfd7d9318bf735ba0f938b8762c5
199oz <chengjieern@gmail.com>
2024-08-26 21:45:29 +0800

    Style: black theme styled

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-let numbers = document.querySelectorAll('.numbers')
-let operators = document.querySelectorAll('.operators')
-let buttons = document.querySelectorAll('button')
-let equals = document.querySelector('.equals')
-let result = document.querySelector('.result')
-let audio = document.querySelector('audio')
-let allClear = document.querySelector('.AC')
+// let numbers = document.querySelectorAll('.numbers')
+// let operators = document.querySelectorAll('.operators')
+// let buttons = document.querySelectorAll('button')
+// let equals = document.querySelector('.equals')
+// let result = document.querySelector('.result')
+// let audio = document.querySelector('audio')
+// let allClear = document.querySelector('.AC')
 
-numbers.forEach(element => {
-    element.addEventListener('click', () => {
-        result.innerHTML += element.textContent
-    })
-});
-operators.forEach(element => {
-    element.addEventListener('click', () =>{
-        result.innerHTML += element.textContent
-    })
-});
-allClear.addEventListener('click',() => {
-    result.innerHTML =""
-})
+// numbers.forEach(element => {
+//     element.addEventListener('click', () => {
+//         result.innerHTML += element.textContent
+//     })
+// });
+// operators.forEach(element => {
+//     element.addEventListener('click', () =>{
+//         result.innerHTML += element.textContent
+//     })
+// });
+// allClear.addEventListener('click',() => {
+//     result.innerHTML =""
+// })
 
-equals.addEventListener('click',() =>{
-    if (result.innerHTML.includes('+')) {
-        result.innerHTML = result.innerHTML.split('+').reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
-    }
-    if (result.innerHTML.includes('-')) {
-        result.innerHTML = result.innerHTML.split('-').reduce((a,b)=> a-b )
-    }
-    if (result.innerHTML.includes('%')) {
-        result.innerHTML = result.innerHTML.split('%').reduce((a,b)=> a/b )
-    }
-    if (result.innerHTML.includes('X')) {
-        result.innerHTML = result.innerHTML.split('X').reduce((a,b)=> a*b )
-    }
+// equals.addEventListener('click',() =>{
+//     if (result.innerHTML.includes('+')) {
+//         result.innerHTML = result.innerHTML.split('+').reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+//     }
+//     if (result.innerHTML.includes('-')) {
+//         result.innerHTML = result.innerHTML.split('-').reduce((a,b)=> a-b )
+//     }
+//     if (result.innerHTML.includes('%')) {
+//         result.innerHTML = result.innerHTML.split('%').reduce((a,b)=> a/b )
+//     }
+//     if (result.innerHTML.includes('X')) {
+//         result.innerHTML = result.innerHTML.split('X').reduce((a,b)=> a*b )
+//     }
     
-})
+// })
 
-buttons.forEach(element => {
-    element.addEventListener('click',() => {
-        audio.volume= 0.3
-        audio.currentTime = 0
-        audio.play()
-    })
-});
+// buttons.forEach(element => {
+//     element.addEventListener('click',() => {
+//         audio.volume= 0.3
+//         audio.currentTime = 0
+//         audio.play()
+//     })
+// });
+
